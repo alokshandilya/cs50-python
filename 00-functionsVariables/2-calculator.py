45b745a98c29ec5130efff9c2cfc197462f0d3ef
Alok Shandilya <alok.shandilya10@gmail.com>
2024-01-10 03:46:39 +0530

    add 2-calculator.py

---
+++ 00-functionsVariables/2-calculator.py
@@ -0,0 +1,80 @@
+# Demonstrates addition
+x = 1
+y = 2
+z = x + y
+print(z)
+
+# Demonstrates (unintended) concatenation of strings
+# Prompt user for two integers
+x = input("What's x? ")
+y = input("What's y? ")
+z = x + y
+print(z)
+
+# Demonstrates conversion from str to int
+x = input("What's x? ")
+y = input("What's y? ")
+z = int(x) + int(y)
+print(z)
+
+# Demonstrates nesting of function calls
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+z = x + y
+print(z)
+
+# Demonstrates conversion of str to float
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+z = x + y
+print(z)
+
+# Demonstrates rounding to nearest int
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+z = round(x + y)
+print(z)
+
+# Demonstrates fewer variables
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+print(round(x + y))
+
+# Demonstrates formatting with commas
+# TODO
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+z = round(x + y)
+print(f"{z:,}")  # like 17,000,000
+
+# Demonstrates division
+# division with int also returns float
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+z = x / y
+print(z)
+
+# Demonstrates rounding after the decimal point
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+z = round(x / y, 2)
+print(z)
+
+# Demonstrates formatting after the decimal place
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+z = x / y
+print(f"{z:.2f}")
+
+
+# Demonstrates defining a function with a return value
+def main():
+    x = int(input("What's x? "))
+    print("x squared is", square(x))
+
+
+def square(n):
+    return n * n
+
+
+main()
