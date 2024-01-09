5fa4635bc50b021502840376f30e0d23a1a241f0
Alok Shandilya <alok.shandilya10@gmail.com>
2024-01-09 20:30:57 +0530

    Create 1-hello.py

---
+++ src0/1-hello.py
@@ -0,0 +1,76 @@
+# Demonstrates a function with a positional argument
+print("hello, world")
+
+# Demonstrates a function with a positional argument and a return value
+name = input("What's your name? ")
+print("hello,")
+print(name)
+
+# Demonstrates concatenation of strings
+name = input("What's your name? ")
+print("hello, " + name)
+
+# Demonstrates a function with two positional arguments
+name = input("What's your name? ")
+print("hello,", name)
+
+# Demonstrates a function with a positional argument and a named argument
+name = input("What's your name? ")
+print("hello, ", end="")
+print(name)
+
+# Demonstrates a format string
+name = input("What's your name? ")
+print(f"hello, {name}")
+
+# Demonstrates str functions
+name = input("What's your name? ").strip().title()
+print(f"hello, {name}")
+
+# Demonstrates str functions
+name = input("What's your name? ").strip().title()
+first, last = name.split(" ")
+print(f"hello, {first}")
+
+
+# Demonstrates defining a function without parameters
+def hello():
+    print("hello")
+
+
+name = input("What's your name? ")
+hello()
+print(name)
+
+
+# Demonstrates defining a function with a parameter
+def hello1(to):
+    print("hello,", to)
+
+
+name = input("What's your name? ")
+hello1(name)
+print("#############################")
+
+
+# Demonstrates defining a function with a parameter with a default value
+def hello2(to="world"):
+    print("hello,", to)
+
+
+hello2()
+name = input("What's your name? ")
+hello2(name)
+
+
+# Demonstrates defining a main function
+def main():
+    name = input("What's your name? ")
+    hello3(name)
+
+
+def hello3(to="world"):
+    print("hello,", to)
+
+
+main()
