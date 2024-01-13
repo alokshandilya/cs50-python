dedb1ac38ab80120655bc86d9c5da3ab4e5fde5a
Alok Shandilya <alok.shandilya10@gmail.com>
2024-01-13 06:39:03 +0530

    add notes
    - match [switch for some other lagugages]

---
+++ 01-conditionals/4-house.py
@@ -0,0 +1,53 @@
+# Compares multiple strings with if/elif/else
+
+name = input("What's your name? ")
+
+if name == "Harry":
+    print("Gryffindor")
+elif name == "Hermione":
+    print("Gryffindor")
+elif name == "Ron":
+    print("Gryffindor")
+elif name == "Draco":
+    print("Slytherin")
+else:
+    print("Who?")
+
+# Uses or
+
+name = input("What's your name? ")
+
+if name == "Harry" or name == "Hermione" or name == "Ron":
+    print("Gryffindor")
+elif name == "Draco":
+    print("Slytherin")
+else:
+    print("Who?")
+
+# Uses match with case
+
+name = input("What's your name? ")
+
+match name:
+    case "Harry":
+        print("Gryffindor")
+    case "Hermione":
+        print("Gryffindor")
+    case "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")
+
+# Uses |
+
+name = input("What's your name? ")
+
+match name:
+    case "Harry" | "Hermione" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")
