9309781a748410bff66db3ed42b8a9a0cd9e954c
Alok Shandilya <alok.shandilya10@gmail.com>
2024-01-14 02:05:42 +0530

    add notes week2
    - hogwarts.py
    - mario.py

---
+++ 02-loops/2-hogwarts.py
@@ -0,0 +1,53 @@
+# Demonstrates indexing into a list
+students = ["Hermione", "Harry", "Ron"]
+
+print(students[0])
+print(students[1])
+print(students[2])
+
+# Demonstrates iterating over a list
+students = ["Hermione", "Harry", "Ron"]
+
+for student in students:
+    print(student)
+
+# Demonstrates iterating over and indexing into a list
+students = ["Hermione", "Harry", "Ron"]
+
+for i in range(len(students)):
+    print(i + 1, students[i])
+
+# Demonstrates indexing into a dict
+students = {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin",
+}
+
+print(students["Hermione"])
+print(students["Harry"])
+print(students["Ron"])
+print(students["Draco"])
+
+# Demonstrates iterating over and index into a dict
+students = {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin",
+}
+
+for student in students:
+    print(student, students[student], sep=", ")
+
+# Demonstrates iterating over a list of dict objects
+students = [
+    {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russell terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None},
+]
+
+for student in students:
+    print(student["name"], student["house"], student["patronus"], sep=", ")
---
+++ 02-loops/3-mario.py
@@ -0,0 +1,90 @@
+# Prints a column of bricks
+print("#")
+print("#")
+print("#")
+
+# Prints column of bricks using a loop
+for _ in range(3):
+    print("#")
+
+
+# Prints column of bricks using a function with a loop
+def main():
+    print_column(3)
+
+
+def print_column(height):
+    for _ in range(height):
+        print("#")
+
+
+main()
+
+
+# Prints column of bricks using a function with str multiplication
+def main2():
+    print_column2(3)
+
+
+def print_column2(height):
+    print("#\n" * height, end="")
+
+
+main2()
+
+
+# Prints row of coins using a function with str multiplication
+def main3():
+    print_row3(4)
+
+
+def print_row3(width):
+    print("?" * width)
+
+
+main3()
+
+
+# Prints square of bricks using a function with nested loops
+def main4():
+    print_square4(3)
+
+
+def print_square4(size):
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
+
+
+main4()
+
+
+# Prints square of bricks using a function with a loop and str multiplication
+def main5():
+    print_square5(3)
+
+
+def print_square5(size):
+    for _ in range(size):
+        print("#" * size)
+
+
+main5()
+
+
+# Prints square of bricks using a function with a loop and str multiplication
+def main6():
+    print_square6(3)
+
+
+def print_square6(size):
+    for _ in range(size):
+        print_row6(size)
+
+
+def print_row6(width):
+    print("#" * width)
+
+
+main6()
