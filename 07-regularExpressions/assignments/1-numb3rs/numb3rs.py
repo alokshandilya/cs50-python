5233ea02465a3794bf21c62241e974e20e854205
Alok Shandilya <alok.shandilya10@gmail.com>
2024-04-21 13:27:56 +0530

    add problem 1
    - week 7
    - numb3rs.py

---
+++ 07-regularExpressions/assignments/1-numb3rs/numb3rs.py
@@ -0,0 +1,19 @@
+import re
+
+
+def main():
+    print(validate(input("IPv4 Address: ")))
+
+
+def validate(ip):
+    if re.search(
+        r"^(([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])$",
+        ip,
+    ):
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    main()
